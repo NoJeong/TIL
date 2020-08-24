450ff8177d02f037603e615d5dd497c1b338a167
Nojeong <op032@naver.com>
2020-08-24 10:03:32 +0900

    0824 replace

--- TID/20.07.19.BASEBALL.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-url = 'https://www.koreabaseball.com/Record/Player/HitterBasic/Basic1.aspx'
-
-table = pd.read_html(url) #테이블을 가져오는 기능
-
-#len(table) #테이블 갯수를 알아보는 기능 
-print(table[0])
-
